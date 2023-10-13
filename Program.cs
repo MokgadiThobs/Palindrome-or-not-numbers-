b2554d76414a0176893ecd167e87bcebc72d50d0
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-13 04:43:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_or_not_numbers_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if it's a palindrome:");
+            int number = int.Parse(Console.ReadLine());
+            int temp = number;
+            int reversedNumber = 0;
+            while (temp > 0)
+            {
+                int remainder = temp % 10;
+                reversedNumber = (reversedNumber * 10) + remainder;
+                temp /= 10;
+            }
+            if (number == reversedNumber)
+            {
+                Console.WriteLine($"{number} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a palindrome.");
+            }
+        }
+    }
+
+}
